5f5db8e2a5ff2ea58542adf8083ea9a94e73967e
Adolfo Gabazutt <adolfo.gbztt@gmail.com>
2023-11-07 21:11:27 -0500

    feat:

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,5 @@
+import { ChakraProvider, Box, Heading, extendTheme, cookieStorageManagerSSR } from "@chakra-ui/react";
+import React, { useContext, useEffect, useMemo } from 'react'
 import {
   Links,
   LiveReload,
@@ -5,47 +7,165 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData
 } from "@remix-run/react";
+import { withEmotionCache } from '@emotion/react'
 
-import type { LinksFunction } from "@remix-run/node"; // or cloudflare/deno
-
-// import styles from "~/styles/dashboard.css";
-
-export const links: LinksFunction = () => [
-  { rel: "stylesheet", href: 'https://vikdiesel.github.io/admin-one-bulma-dashboard/css/main.min.css', type: 'text/css' },
-  // { rel: "stylesheet", href: 'https://vikdiesel.github.io/admin-one-bulma-dashboard/css/main.min.css', type: 'text/css' },
-];
+import { ServerStyleContext, ClientStyleContext } from './context'
 
 
-// import bulma from "bulma/css/bulma.css?inline";
-// import adminone from "~/styles/styles.css?inline";
 
-<link rel="stylesheet" href="css/main.min.css"></link>
+
+interface DocumentProps {
+  children: React.ReactNode;
+}
+
+const Document = withEmotionCache(({ children }: DocumentProps, emotionCache) => {
+
+  const colors = {
+    brand: {
+      900: '#1a365d',
+      800: '#153e75',
+      700: '#2a69ac',
+    }
+  }
+
+  const theme = extendTheme({ colors })
+
+  // here we can set the default color mode. If we set it to null,
+  // there's no way for us to know what is the the user's preferred theme
+  // so the cient will have to figure out and maybe there'll be a flash the first time the user visits us.
+  const DEFAULT_COLOR_MODE: "dark" | "light" | null = 'dark';
+
+  const CHAKRA_COOKIE_COLOR_KEY = "chakra-ui-color-mode";
+
+  function getColorMode(cookies: string) {
+    if (cookies == null) {
+      return null;
+    }
+    const match = cookies.match(new RegExp(`(^| )${CHAKRA_COOKIE_COLOR_KEY}=([^;]+)`));
+    return match == null ? void 0 : match[2];
+  }
+
+
+
+  let cookies = useLoaderData<string>()
+
+  // the client get the cookies from the document 
+  // because when we do a client routing, the loader can have stored an outdated value
+  if (typeof document !== "undefined") {
+    cookies = document.cookie;
+  }
+
+  // get and store the color mode from the cookies.
+  // It'll update the cookies if there isn't any and we have set a default value
+  let colorMode = useMemo(() => {
+    let color = getColorMode(cookies)
+
+    if (!color && DEFAULT_COLOR_MODE) {
+      cookies += ` ${CHAKRA_COOKIE_COLOR_KEY}=${DEFAULT_COLOR_MODE}`;
+      color = DEFAULT_COLOR_MODE;
+    }
+
+    return color
+  }, [cookies]);
+
+
+
+  const serverStyleData = useContext(ServerStyleContext);
+  const clientStyleData = useContext(ClientStyleContext);
+
+  // Only executed on client
+  useEffect(() => {
+    // re-link sheet container
+    emotionCache.sheet.container = document.head;
+    // re-inject tags
+    const tags = emotionCache.sheet.tags;
+    emotionCache.sheet.flush();
+    tags.forEach((tag) => {
+      (emotionCache.sheet as any)._insertTag(tag);
+    });
+    // reset cache to reapply global styles
+    clientStyleData?.reset();
+  }, []);
+
+
+
+  return (
+    <html
+      lang="en"
+      {...colorMode
+      && {
+        "data-theme": colorMode,
+        "style": { colorScheme: colorMode },
+      }
+      }
+    >
+      <head>
+        <Meta />
+        <Links />
+        {serverStyleData?.map(({ key, ids, css }) => (
+          <style
+            key={key}
+            data-emotion={`${key} ${ids.join(" ")}`}
+            dangerouslySetInnerHTML={{ __html: css }}
+          />
+        ))}
+      </head>
+      <body
+        {...colorMode && {
+          className: `chakra-ui-${colorMode}`
+        }}
+      >
+        <ChakraProvider
+          colorModeManager={cookieStorageManagerSSR(cookies)}
+          theme={theme}
+        >
+          {children}
+        </ChakraProvider>
+
+        <ScrollRestoration />
+
+        <Scripts />
+
+        <LiveReload />
+      </body>
+    </html>
+  )
+});
 
 
 export default function App() {
-  return (
-    <html lang="en" className="has-aside-left has-aside-mobile-transition has-navbar-fixed-top has-aside-expanded">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        {/* <style>{adminone}</style>
-        <style>{bulma}</style> */}
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <div className="container is-fluid">
-          <div id="app">
-            <Outlet />
-            <ScrollRestoration />
-            <LiveReload />
-            <Scripts />
-            <script type='module' src="https://vikdiesel.github.io/admin-one-bulma-dashboard/js/main.min.js"></script>
-            <link rel="stylesheet" href="https://cdn.materialdesignicons.com/4.9.95/css/materialdesignicons.min.css"></link>
-          </div>
-        </div>
-      </body>
-    </html>
-  );
+  return (<Document><Outlet /></Document>);
 }
+// How ChakraProvider should be used on CatchBoundary
+// export function CatchBoundary() {
+//   const caught = useCatch();
+
+//   return (
+//     <Document title={`${caught.status} ${caught.statusText}`}>
+//       <ChakraProvider>
+//         <Box>
+//           <Heading as="h1" bg="purple.600">
+//             [CatchBoundary]: {caught.status} {caught.statusText}
+//           </Heading>
+//         </Box>
+//       </ChakraProvider>
+//     </Document>
+//   );
+// }
+
+// How ChakraProvider should be used on ErrorBoundary
+// export function ErrorBoundary({ error }: { error: Error }) {
+//   return (
+//     <Document title="Error!">
+//       <ChakraProvider>
+//         <Box>
+//           <Heading as="h1" bg="blue.500">
+//             [ErrorBoundary]: There was an error: {error.message}
+//           </Heading>
+//         </Box>
+//       </ChakraProvider>
+//     </Document>
+//   );
+// }
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,17 +1,9 @@
-import type { MetaFunction } from "@remix-run/node";
-
-
-export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
-};
+import { Box } from "@chakra-ui/react";
 
 export default function Index() {
   return (
-    <>
-      {'Inicio'}
-    </>
+    <Box bg="tomato" w="100%" p={4} color="white">
+      Hello World!
+    </Box>
   );
 }
