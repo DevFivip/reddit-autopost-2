d5bdf862cf1757cb4870a40622a27f55b6c4a550
Adolfo Gabazutt <adolfo.gbztt@gmail.com>
2023-11-11 19:04:25 -0500

    feat:

--- app/components/clientes/asignarSubreddit.tsx
+++ app/components/clientes/asignarSubreddit.tsx
@@ -15,14 +15,17 @@ import {
     VisuallyHiddenInput,
     Select,
     List,
-    ListItem
+    ListItem,
+    Badge,
+    Icon,
+    Checkbox
 } from '@chakra-ui/react'
 
 import {
     MdOutlineEmail
 } from 'react-icons/md'
 
-import { BsPerson, BsReddit, BsImage, BsTelegram } from 'react-icons/bs'
+import { BsPerson, BsReddit, BsImage, BsTelegram, BsFillPatchCheckFill } from 'react-icons/bs'
 
 import { AuthUser } from 'prisma/types/user';
 // import { Customer } from '@prisma/client';
@@ -46,49 +49,18 @@ export const ComponentAsignarSubredditFormulario: React.FC<TypeComponentAsignarS
         {/* <VisuallyHiddenInput type="number" name='status' id='status' defaultValue={false ? 1 : 0} onChange={handleInputChange} /> */}
 
         <Stack direction={['column', 'row']} spacing='50px'>
-            <Box width={'md'} borderWidth='1px' borderRadius='lg' overflow='hidden'>
+            <Box width={'lg'} borderWidth='1px' borderRadius='lg' overflow='hidden'>
                 <Box m={8}>
+
                     <VStack spacing={5}>
-                        <Heading as={'h2'} size={'1x'}>Datos del Cliente</Heading>
-                        <FormControl id="firstName">
-                            <FormLabel>Nombre</FormLabel>
-                            <InputGroup >
-                                <InputLeftElement pointerEvents="none">
-                                    <BsPerson />
-                                </InputLeftElement>
-                                <Input type="text" name='firstName' id='firstName' defaultValue={false || ''} onChange={handleInputChange} />
-                            </InputGroup>
-                        </FormControl>
-                        <FormControl id="lastName">
-                            <FormLabel>Apellido</FormLabel>
-                            <InputGroup >
-                                <InputLeftElement pointerEvents="none">
-                                    <BsPerson />
-                                </InputLeftElement>
-                                <Input type="text" name='lastName' id='lastName' defaultValue={false || ''} onChange={handleInputChange} />
-                            </InputGroup>
-                        </FormControl>
-
-                        <FormControl id="email">
-                            <FormLabel>Mail</FormLabel>
-                            <InputGroup >
-                                <InputLeftElement pointerEvents="none">
-                                    <MdOutlineEmail />
-                                </InputLeftElement>
-                                <Input type="text" name='email' id='email' defaultValue={false || ''} onChange={handleInputChange} />
-                            </InputGroup>
-                        </FormControl>
-
-                        <FormControl id="tags">
-                            <FormLabel>Tags (separar por comas)</FormLabel>
-                            <InputGroup >
-                                <InputLeftElement pointerEvents="none">
-                                    <BsPerson />
-                                </InputLeftElement>
-                                <Input type="text" name='tags' id='tags' defaultValue={false || ''} onChange={handleInputChange} />
-                            </InputGroup>
-                        </FormControl>
+                        <Heading as={'h2'} size={'1x'}>Asignar Subreddits</Heading>
                     </VStack>
+
+                    <List spacing={3}>
+                        {subredditOnCustomerEditar?.map((sr) => <ListItem borderWidth='1px' borderRadius='lg' p={2}> <Checkbox defaultChecked={sr?.status} id='verificacion' name='verificacion' value={'requiere'}>{sr.nombre}  {!sr.verificacion || <Icon as={BsFillPatchCheckFill} color='teal.500'></Icon>}  {sr.tags?.split(',').map((t, k) => <Badge key={k} m={2} colorScheme='purple'>{t}</Badge>)} </Checkbox> </ListItem>)}
+                    </List>
+
+
                 </Box>
             </Box>
 
